d182f628667d2b641909de2e40fa2a272ac9fea9
Mátyás Dávid András <andras.matyas@edu.bme.hu>
2022-03-30 17:38:41 +0200

    setup

--- public/App.js
+++ public/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
-
-// #region Firebase
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -23,7 +21,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-// #endregion
 
 function App() {
   return (
@@ -31,8 +28,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Matyi!
+          Edit <code>src/App.js</code> and save to reload.
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
